e28f63d93f0837f499546203fe839428a897d30d
Kai Pei <peik@carleton.edu>
2017-09-16 18:36:38 -0500

    testing socket.io

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
+//var express = require('express'),
+    //app     = express(),
+    //morgan  = require('morgan');
+
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+//var fs = require('fs');
+
     
 Object.assign=require('object-assign')
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+//app.engine('html', require('ejs').renderFile);
+//app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -56,7 +61,7 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -72,9 +77,9 @@ app.get('/', function (req, res) {
   } else {
     res.render('index.html', { pageCountMessage : null});
   }
-});
+});*/
 
-app.get('/pagecount', function (req, res) {
+/*app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -87,19 +92,25 @@ app.get('/pagecount', function (req, res) {
   } else {
     res.send('{ pageCount: -1 }');
   }
-});
+});*/
 
 // error handling
-app.use(function(err, req, res, next){
+/*app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
-});
+});*/
 
 initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
-app.listen(port, ip);
+/*app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+module.exports = app ;*/
+
+app.listen(port);
+
+io.on('connection', function (socket) {
+    console.log("it worked");
+});
