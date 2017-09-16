1bcd1516811b8be193573dae2964baa37b9c9541
Kai Pei <peik@carleton.edu>
2017-09-16 18:38:58 -0500

    testing socket.io

--- server.js
+++ server.js
@@ -111,6 +111,19 @@ module.exports = app ;*/
 
 app.listen(port);
 
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
 io.on('connection', function (socket) {
     console.log("it worked");
 });
