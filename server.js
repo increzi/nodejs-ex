3ab15e73d00159796ab2ac9833849ccbfc63e02a
Kai Pei <peik@carleton.edu>
2017-09-17 14:03:04 -0500

    chat app example

--- server.js
+++ server.js
@@ -102,11 +102,10 @@ initDb(function(err){
 
 //app.listen(port);
 
-io.on('connect', function (socket) {
-    console.log('a user connected');
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-    });
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(port);
