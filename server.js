d4c461039d1f42c71f9eb6cf552a7083576fbc09
Kai Pei <peik@carleton.edu>
2017-09-16 20:21:38 -0500

    test msges

--- server.js
+++ server.js
@@ -109,6 +109,7 @@ initDb(function(err){
 //app.listen(port);
 
 io.on('connection', function (socket) {
+    console.log("sent from server")
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
         console.log(data);
