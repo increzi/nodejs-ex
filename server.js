857ff2681661e4832acccc7721f7de771219477e
Kai Pei <peik@carleton.edu>
2017-09-17 14:19:34 -0500

    testing output

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ initDb(function(err){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', "hehe");
   });
 });
 
